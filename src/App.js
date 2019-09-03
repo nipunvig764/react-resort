d3f8637f92c0e7f4001566672eab31cfcf51422f
nipunvig <nipunvig764@gmail.com>
2019-09-03 15:52:43 -0400

    resort application added

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import "./App.css";
+
+import Home from "./pages/home";
+import Rooms from "./pages/room";
+import Error from "./pages/Error";
+import SingleRoom from "./pages/SingleRoom";
+import {Route,Switch} from 'react-router-dom';
+import Navbar from "./components/Navbar";
+
+export default function App() {
+  return(
+    <>
+    <Navbar />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/rooms/" component={Rooms} />
+      <Route exact path="/rooms/:slug" component={SingleRoom} />
+      <Route component={Error} />
+    </Switch>
+    </>
+  )
+}
+
---
+++ src/pages/Error.js
@@ -0,0 +1,11 @@
+// import React from 'react';
+// import Hero from "../components/Hero";
+// export default function Error(){
+//     return <Hero>
+//         <Banner title='404' subtitle='page not found'>
+//             <Link to='/' className='btn-primary'>
+//                 Return Home
+//             </Link>
+//         </Banner>
+//         </Hero>;
+// }
